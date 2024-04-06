20d582678d82be44d51b0aa3c29a3cc4dde393c3
Keerthana <surji73@gmail.com>
2024-04-06 22:32:06 +0530

    second commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 app.use(cors());
 app.use(bodyParser.json());
